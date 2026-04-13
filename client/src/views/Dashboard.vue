3f67ecd670c5a702bd9475714d3727e0e95c7002
Alexander van Grootel <alexvangrootel@alexander-van-grootel-macbook.tail2dda0.ts.net>
2026-04-13 12:42:25 -0700

    Fix Create/View PO button using wrong field on backlog items
    The template checked item.purchase_order_id but /api/backlog returns
    has_purchase_order. Use the real field so backlog items that already have
    a PO show "View PO" on initial load instead of "Create PO".

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -210,7 +210,7 @@
                   </td>
                   <td>
                     <button
-                      v-if="!item.purchase_order_id"
+                      v-if="!item.has_purchase_order"
                       @click.stop="openPOModal(item)"
                       class="po-button create"
                     >
@@ -666,6 +666,7 @@ export default {
       // Update the backlog item with the new PO ID
       const item = allBacklogItems.value.find(b => b.id === poData.backlog_item_id)
       if (item) {
+        item.has_purchase_order = true
         item.purchase_order_id = poData.id
         item.purchase_order = poData
       }
